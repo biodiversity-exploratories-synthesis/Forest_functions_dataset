804004dc34e47d31886f30d1b4c43311068d1bec
Persilian <59728330+Persilian@users.noreply.github.com>
2023-08-15 11:25:15 +0200

    minor update

--- forest_function_helper.R
+++ forest_function_helper.R
@@ -12,19 +12,24 @@
 
 ### === working directory === ### 
 setwd("C:/Users/Marc Beringer/Desktop/R/Synthesis_dataset_functions_forest")
+### ===== ###
 
 ### === libraries === ###
 library(tidyverse)
+### ===== ###
 
 ### === read metadata table === ###
 # the input file is the metadata table of the upcoming forest functions synthesis dataset (not yet uploaded on BExIS).
 # Please download the forest functions synthesis dataset metadata into your R working directory
 #USER : insert path to "synthesis_forest_function_metadata_HIVE_local_copy.xlsx"
 forest_fun_metadata <- BE_synthesis_forest_dat <- read.table("synthesis_forest_function_metadata_HIVE_local_copy.xlsx", header = T, sep = "\t")
+### ===== ###
 
 ### === select the necessary columns and save output to working directory === ###
 helper_dat <- forest_fun_metadata %>% 
   subset( select = c("ColumnName", "AggregatedColumnName", "codedYear", "dataID", "Dataset Version", "Projectname"))
 
-write.table(helper_dat, file = "forest_functions_helper.txt", quote = F, sep = "\t", row.names = F) 
+write.table(helper_dat, file = "forest_functions_helper.txt", quote = F, sep = "\t", row.names = F)
+### ===== ###
+
 
